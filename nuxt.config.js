2b6681de08edff8de5e74870370905298b463b24
allowedcloud <79382276+allowedcloud@users.noreply.github.com>
2021-04-20 14:43:56 -0400

    Forgot comma

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import awsconfig from './src/aws-exports.js'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  target: 'static'
+  target: 'static',
   head: {
     title: 'incident-map',
     htmlAttrs: {
