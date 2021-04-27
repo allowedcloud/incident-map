f300d5c6e55d49bb5d989c03d94534ee599a6633
allowedcloud <79382276+allowedcloud@users.noreply.github.com>
2021-04-27 14:44:31 -0400

    Update about

--- components/About.vue
+++ components/About.vue
@@ -1,13 +1,17 @@
 <template>
-  <p class="mt-20 px-6">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel
-    pellentesque nisi, vel dapibus sapien. Donec pharetra felis eu velit euismod
-    dictum. Mauris elementum efficitur ipsum, id vulputate nunc imperdiet nec.
-    Nullam vitae libero sed tortor lobortis maximus. Mauris euismod, mi a
-    condimentum cursus, mi nulla consectetur dui, at suscipit neque tellus
-    mattis mauris. Aliquam neque dui, consequat eu enim at, fermentum luctus
-    ipsum. Sed id congue tortor. In dignissim sed urna faucibus pharetra.
-  </p>
+  <div>
+    <p class="mt-20 px-10 text-xl">
+      The <span class="text-yellow-500">Significant Activities Map</span> (SAM) is a tool
+      to track organized-crime related activities in Mexico. This list is curated by hand,
+      with all source links provided on the right of each activity. The majority of primary sources will
+      be from social media and supplemented with press sources when applicable. Selecting a month
+      under the date tab will filter the list by month.
+    </p>
+    <p class="mt-8 px-10">
+      SAM is built with Vue JS and uses Tailwind CSS for styling. You can check out the
+      <a href="https://github.com/" class="text-blue-400 underline"> source code</a> if you're curious.
+    </p>
+  </div>
 </template>
 
 <script>
