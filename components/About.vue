9fd4e595b503399b09c886a13bbbbbc075b3c6fb
allowedcloud <79382276+allowedcloud@users.noreply.github.com>
2021-04-27 16:54:43 -0400

    Add number of activities listed to Info component

--- components/About.vue
+++ components/About.vue
@@ -2,9 +2,9 @@
   <div>
     <p class="mt-20 px-10 text-xl">
       The <span class="text-yellow-500">Significant Activities Map</span> (SAM) is a tool
-      to track organized-crime related activities in Mexico. This list is curated by hand,
-      with all source links provided on the right of each activity. The majority of primary sources will
-      be from social media and supplemented with press sources when applicable. Selecting a month
+      to track organized-crime related activity in Mexico. This list is curated by hand,
+      with all source links provided on the right of each activity. The majority of primary sources are
+      from social media and supplemented with press sources when applicable. Selecting a month
       under the date tab will filter the list by month.
     </p>
     <p class="mt-8 px-10">
--- components/Info.vue
+++ components/Info.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="absolute top-3 left-14">
-    Month: <span class="font-bold">{{ selectedMonth }}</span>
+    <span class="font-bold">
+      {{ numberOfActivities }}
+    </span>
+    &nbsp; activities in 
+    <span class="font-bold">
+      {{ selectedMonth }}
+    </span>
   </div>
 </template>
 
@@ -9,6 +15,10 @@ export default {
   computed: {
     selectedMonth() {
       return this.$store.state.incidents.selectedMonth
+    },
+    numberOfActivities() {
+      const count = this.$store.state.incidents.sortedByMonth
+      return count.length
     }
   }
 }
